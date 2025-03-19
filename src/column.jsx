f30564366bc8786e8aae8ae9ffb06ea5b588e426
nof1eld <imadeddine799@gmail.com>
2025-03-19 15:17:50 +0100

    Edit icon path

--- src/column.jsx
+++ src/column.jsx
@@ -1,7 +1,7 @@
 export function Column(num, icon, title, text) {
   return (
     <div className="column" id={`column-${num}`}>
-      <img src={`src/images/${icon}`} alt={title} />
+      <img src={`images/${icon}`} alt={title} />
       <h2>{title}</h2>
       <p>{text}</p>
       <button>Learn More</button>
